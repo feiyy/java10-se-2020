1bb1431e97bcfecb307ef04231567f9045db9a59
you <you@neusoft.com>
2020-07-30 15:37:41 +0800

    update practice1

--- src/com/neuedu/test/chapter2/Practice1.java
+++ src/com/neuedu/test/chapter2/Practice1.java
@@ -6,16 +6,7 @@ public class Practice1 {
 		// TODO Auto-generated method stub
 		//已知a,b均是整型变量，写出将a,b两个变量中的值互换的程序。
 		
-		int a = 100;
-		int b = 200;
-		
-		int c = a;//c=100
-		a = b; //a = 200	
-		b = c;//b = 100;
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println("a="+a+",b="+b);
+		System.out.println("xxxx");
 		
 
 	}
