a8b76ac6be7de078ae9bccf7a3d6ed695e8cd005
you <you@neusoft.com>
2020-07-30 15:24:17 +0800

    update howlloworld

--- src/com/neuedu/test/chapter2/HelloWorld.java
+++ src/com/neuedu/test/chapter2/HelloWorld.java
@@ -15,6 +15,7 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Hello China");
+		System.out.println("Hello You");
 	}
 
 }
